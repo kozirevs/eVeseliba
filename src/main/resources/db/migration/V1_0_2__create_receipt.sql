564a348217f4a7633692074fe00f473d6b54a714
akoz <kozirevs@inbox.lv>
2020-08-23 14:38:59 +0300

    add method createReceipt

--- src/main/resources/db/migration/V1_0_2__create_receipt.sql
+++ src/main/resources/db/migration/V1_0_2__create_receipt.sql
@@ -9,4 +9,8 @@ CREATE TABLE IF NOT EXISTS receipt (
 );
 
 INSERT INTO receipt (content, is_active, date_from, date_to, doctor_user_pk, patient_user_pk) VALUES
-('')
+('aspirin', true, '2020-08-23T11:07:19.318+0000', '2020-09-23T11:07:19.318+0000', 2, 1),
+('analgin', true, '2020-08-12T11:07:19.318+0000', '2020-09-12T11:07:19.318+0000', 2, 3),
+('paracetamol', true, '2020-08-16T11:07:19.318+0000', '2020-09-16T11:07:19.318+0000', 2, 4),
+('ibuprofen', false, '2020-06-23T11:07:19.318+0000', '2020-07-23T11:07:19.318+0000', 2, 5),
+('antibiotic', true, '2020-08-01T11:07:19.318+0000', '2020-09-01T11:07:19.318+0000', 2, 1),
